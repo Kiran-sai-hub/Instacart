72eb4e61a0cef0d475d4dea11172ec9b6594dbbd
Kiran-sai-hub <saikiran.vonteddu@gmail.com>
2025-10-18 23:22:12 +0530

    implimented Stripe for secure checkouts.

--- api/server.js
+++ api/server.js
@@ -7,6 +7,7 @@ import authRoutes from "./routes/auth.routes.js";
 import productRoutes from "./routes/product.routes.js";
 import cartRoutes from "./routes/cart.routes.js";
 import couponRoutes from "./routes/coupon.routes.js";
+import paymentRoutes from "./routes/payment.routes.js";
 
 //utils
 import connectDB from "./lib/db.js";
@@ -22,6 +23,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
 
 connectDB()
   .then(() => {
